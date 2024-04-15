921a69f03c634d15eed121cae3b3c68a47e52e80
umairaslam2 <154333213+umairaslam2@users.noreply.github.com>
2024-04-15 20:28:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,267 @@
+//""
+//h
+//g
+
+
+
+//chapter 6-9
+
+
+//question 1;
+
+// var abc = prompt("Enter your city");
+
+// if(abc=="Karachi"){
+//     alert("Welcome to city of lights")
+// }
+
+// question 2;
+
+// var abc = prompt("Enter your gender");
+
+// if(abc=="male"){
+//     alert("Good Morning Sir")
+// }
+// if(abc=="female"){
+//     alert("Good Morning Ma’am")
+// }
+
+// question 3 ;
+
+// var abc = prompt("Enter color of road traffic signal");
+
+// if(abc=="Red"){
+//     alert("Must Stop")
+// }
+// if(abc=="Yellow"){
+//     alert("Ready to move")
+// }
+// if(abc=="Green"){
+//     alert("Move now")
+// }
+
+// question 4 ;
+
+// var abc = prompt("Enter remaining fuel in car");
+
+// if(abc < 0.25 + "liters" ){
+//     alert("Please refill the fuel in your car")
+// }
+
+// question 5 ;
+   
+   //(A)
+
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true")
+// }
+
+  //(B);
+
+//   var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true")
+// }
+
+   // (c);
+
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true")
+// }
+
+
+// question 6;
+
+// var abc = prompt("Enter your percentage");
+
+//  if(abc>=80){
+//      alert("grade" + "= " + "A-one" + " ;" +  " "  + "Remarks" + "=" + "Excellent")
+//  }
+
+//  if(abc>=70){
+//      alert("grade" + "= " + "A" + " ;" +  " "  + "Remarks" + "=" + "good")
+     
+// }
+
+// if(abc>=60){
+//     alert("grade" + "= " + "B" + " ;" +  " "  + "Remarks" + "=" + "You need to improve")
+// }
+
+// if(abc<60){
+//     alert("grade" + "= " + "Fail" + " ;" +  " "  + "Remarks" + "=" + "Sorry")
+// }
+
+
+// question 7 ;
+
+// 
+// var abc = prompt("Enter any number from 1 to 10");
+
+//  if(abc=1){
+//      alert("correct")
+//  }
+
+//  if(abc=2){
+//      alert("Close enough to the correct answer")
+
+     
+// }
+
+// Chapter 12-13
+
+
+        //Q1
+
+        // var a = 1;
+
+
+        //Q2
+
+        // var Val1 = +prompt("Enter 1st Value (In Numbers)")
+        // var Val2 = +prompt("Enter 2nd Value (In Numbers)")
+
+        // if(Val1 > Val2)
+        // {
+        //     alert(Val1+" is Greater than "+Val2);
+        // }
+        // else if(Val1 < Val2)
+        // {
+        //     alert(Val2+" is Greater than "+Val1);
+        // }
+        // else if(Val1 === Val2)
+        // {
+        //     alert("Entered both values are equal.");
+        // }
+        // else
+        // {
+        //     alert("Invalid Value.")
+        // }
+
+        //3
+
+        // var Val1 = +prompt("Enter a value (In Numbers)")
+
+        // if(Val1 > 0)
+        // {
+        //     alert("Entered value is positive");
+        // }
+        // else if(Val1 < 0)
+        // {
+        //     alert("Entered value is negative");
+        // }
+        // else if(Val1 === 0)
+        // {
+        //     alert("Entered value is Zero.");
+        // }
+        // else
+        // {
+        //     alert("Invalid Value.")
+        // }
+
+        //4
+
+        // var Val1 = prompt("Enter a vowel")
+        // var vowels = ['a','A','e','E','i','I','o','O','u','U'];
+
+        // if(isNaN(Val1))
+        // {
+        //     if(vowels.includes(Val1))
+        //     {
+        //         alert("Entered value is a vowel")
+        //     }
+        //     else
+        //     {
+        //         alert("Entered value is not a vowel")
+        //     }
+        // }
+        // else
+        // {
+        //     alert("Entered Value is a Number");
+        // }
+
+        //5
+
+        // var Pass = "Password123";
+
+        // var inputPass = prompt("Please enter your password:");
+
+        // if (!inputPass) 
+        // {
+        //     alert("Please reload and enter your password");
+        // }
+        // else if (inputPass === Pass)
+        // {
+        //     alert("Correct! The password you entered matches the original password");
+        // }
+        // else
+        // {
+        //     alert("Incorrect password");
+        // }
+
+
+        //6
+
+
+        // var greeting;
+        // var hour = prompt();
+        // if (hour < 12)
+        // {
+        //     greeting = "Good day";
+        // }
+        // else
+        // {
+        //     greeting = "Good evening";
+        // }
+        // alert(greeting);
+
+        //7
+
+        // var inputTime = prompt("Enter Time like: 1900 = 7pm, 2000 = 8pm");
+
+        // if(inputTime >= 0 && inputTime < 1200)
+        // {
+        //     alert("Good Morning")
+        // }
+        // else if(inputTime >= 1200 && inputTime < 1700)
+        // {
+        //     alert("Good Afternoon")
+        // }
+        // else if(inputTime >= 1700 && inputTime < 2100)
+        // {
+        //     alert("Good Evening")
+        // }
+        // else if(inputTime >= 2100 && inputTime < 2359)
+        // {
+        //     alert("Good Night")
+        // }
+        // else
+        // {
+        //     alert("Invalid Time")
+        // }
+
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
